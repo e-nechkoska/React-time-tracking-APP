e6a55bc4c4323eb2235e2d6beec63f7436483a99
e-nechkoska <e.nechkoska@gmail.com>
2020-10-30 21:24:20 +0100

    add routes

---
+++ server/src/routes/project.ts
@@ -0,0 +1,19 @@
+
+import { Router } from 'express';
+
+import projectController from '../controllers/project';
+// import timeController from '../controllers/time';
+
+const router = Router();
+
+router.get('/projects', projectController.projectList);
+router.get('/projects/:projectId', projectController.projectDetail);
+router.post('/projects', projectController.createProject);
+router.put('/projects/:projectId', projectController.updateProject);
+router.delete('/projects/:projectId', projectController.deleteProject);
+
+// router.get('/projects/:projectId/times', timeController.timeList);
+// router.post('/projects/:projectId/times', timeController.timeCreate);
+// router.delete('/projects/:projectId/times/:timeId', timeController.timeDelete);
+
+export default router;
