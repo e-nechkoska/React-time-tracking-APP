740e3f814fda708c83169e7023d872634a1d36c2
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2020-10-31 23:39:56 +0100

    add updateProjectTimes

--- server/src/repositories/projectList.ts
+++ server/src/repositories/projectList.ts
@@ -1,11 +1,12 @@
+
 import Project from "@models/Project";
 import Time from "@models/Time";
 
-const time1 = new Time(1, 'time1', 120);
-const time2 = new Time(2, 'time2', 100);
-const time3 = new Time(3, 'time3', 200);
-const time4 = new Time(4, 'time4', 300);
-const time5 = new Time(5, 'time5', 400);
+const time1 = new Time('time1', 120);
+const time2 = new Time('time2', 100);
+const time3 = new Time('time3', 200);
+const time4 = new Time('time4', 300);
+const time5 = new Time('time5', 400);
 
 const timeList1 = [time1, time2];
 const timeList2 = [time3];
@@ -18,6 +19,21 @@ const project3 = new Project('pr3', 'desc3', timeList3);
 let PROJECTS = [project1, project2, project3];
 
 export const projectsFromDb = () => PROJECTS;
+
 export const updateProjectsDb = (newProjects: Project[]) => {
   PROJECTS = newProjects;
+}
+
+export const updateProjectTimes = (projectId: number, newTimes: any) => {
+  const project = PROJECTS.find( (project) => {
+      if(project.id === projectId) {
+        return true;
+      } else {
+        return false;
+      }
+  });
+
+  if(project) {
+    project.times = newTimes;
+  }
 }
