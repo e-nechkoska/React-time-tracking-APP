8859498ccc51cb7758d69417a775e89f1c317a84
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2020-10-31 23:41:04 +0100

    add index.ts

---
+++ server/src/controllers/time/index.ts
@@ -0,0 +1,11 @@
+
+import { createTime } from './createTime';
+import { timeList, timeDetail } from './getTime';
+import { deleteTime } from './deleteTime';
+
+export default {
+    createTime,
+    timeList,
+    timeDetail,
+    deleteTime
+}
