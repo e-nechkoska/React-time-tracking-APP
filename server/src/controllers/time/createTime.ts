288e0111a2fd8b24d3c25b50f6fafa7033d20dc6
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2020-10-31 23:39:19 +0100

    add createTime controller

---
+++ server/src/controllers/time/createTime.ts
@@ -0,0 +1,33 @@
+
+import { Request, Response } from 'express';
+import timeRepository  from '@repository/time.repository';
+import Time from '@models/Time';
+import { StatusCodes } from 'http-status-codes';
+
+interface CreateTimeRequest {
+    description: String;
+    amount: number;
+}
+
+const validTimeRequest = ({description, amount}: CreateTimeRequest): boolean => {
+    if(description && amount) {
+        return true;
+    } 
+    return false;
+}
+
+export const createTime = (req: Request, res: Response) => {
+    const projectId = Number(req.params.projectId);
+    const {description, amount} = req.body;
+
+    if(validTimeRequest(req.body)) {
+        const newTime = new Time(
+            description,
+            amount
+        )
+        timeRepository.create(projectId, newTime);
+        res.status(StatusCodes.CREATED).end();
+    } else {
+        res.status(StatusCodes.BAD_REQUEST).end();
+    }
+}
