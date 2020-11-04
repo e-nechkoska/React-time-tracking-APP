740a46c19caae9ba8e1961f30c73459afb9f734d
e-nechkoska <e.nechkoska@gmail.com>
2020-11-04 11:22:32 +0100

    add react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Link, Route } from "react-router-dom";
 import './App.css';
+import { HomePage, ProjectDetailsPage } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/">Home</Link>
+      <Link to="/project">Project Details</Link>
+
+      <Switch>
+        <Route exact={true} path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="/project">
+          <ProjectDetailsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
