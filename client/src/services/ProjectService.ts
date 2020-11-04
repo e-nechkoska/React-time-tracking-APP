2a4e81a738381a628bbdc6b419afbb271df02b1e
e-nechkoska <e.nechkoska@gmail.com>
2020-11-04 11:23:17 +0100

    add ProjectService

---
+++ client/src/services/ProjectService.ts
@@ -0,0 +1,21 @@
+import { Project } from "../models"
+
+export const fetchAllProjects = () => { //: Promise<Project[]>
+  return Promise.resolve(null);
+}
+
+export const fetchProjectById = (projectId: number) => { //: Promise<Project>
+  return Promise.resolve(null);
+}
+
+export const createProject = (project: Project) => {
+
+}
+
+export const updateProject = (project: Project) => {
+
+}
+
+export const deleteProject = (projectId: number) => {
+
+}
