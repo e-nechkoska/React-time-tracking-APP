fa338377d9b2eae87a3ec091f2d1e4bf2d645566
e-nechkoska <e.nechkoska@gmail.com>
2020-11-09 17:17:01 +0100

    add updateProject option

--- client/src/components/ProjectComponent.tsx
+++ client/src/components/ProjectComponent.tsx
@@ -5,14 +5,17 @@ interface Props {
   name: string;
   description: string;
   deleteProject: (id: string) => void;
+  updateProject: (id: string) => void;
 }
 
-export function ProjectComponent(props: Props) {
+export const ProjectComponent = (props: Props) => {
   return (
     <li>
       <span>{props.name}</span>
       <div>{props.description}</div>
-      <button type="button">Edit</button>
+      <button 
+        type="button"
+        onClick={() => props.updateProject(props.id)}>Edit</button>
       <button 
         type="button"
         onClick={() => props.deleteProject(props.id)}>Delete</button>
