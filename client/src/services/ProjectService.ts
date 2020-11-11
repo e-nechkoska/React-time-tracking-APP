a393bd5cc5bbd8a80bb59eebb1e5ae1f4aee7a1b
e-nechkoska <e.nechkoska@gmail.com>
2020-11-11 13:26:54 +0100

    add ProjectService

--- client/src/services/ProjectService.ts
+++ client/src/services/ProjectService.ts
@@ -1,21 +1,32 @@
+//use these services in HomePage.tsx
 import { Project } from "../models"
 
-export const fetchAllProjects = () => { //: Promise<Project[]>
-  return Promise.resolve(null);
+export const fetchAllProjects = (): Promise<Project[]> => {
+  return Promise.resolve(fetch("/projects")
+    .then(res => res.json()));
 }
 
-export const fetchProjectById = (projectId: number) => { //: Promise<Project>
-  return Promise.resolve(null);
+export const fetchProjectById = (projectId: string): Promise<Project> => { 
+  return Promise.resolve(fetch(`/projects/${projectId}`).then(res => res.json()));
 }
 
-export const createProject = (project: Project) => {
+export const createProject = (project: Project): Promise<Project> => {
+  return Promise.resolve(fetch(`/projects`, { 
+    method: "POST",
+    headers: {"Content-type" : "application/json" },
+    body: JSON.stringify(project) 
+  }).then(res => res.json()));
 
 }
 
-export const updateProject = (project: Project) => {
-
+export const updateProject = (projectId: string, project: Project) => {
+  return Promise.resolve(fetch(`/projects/${projectId}`, { 
+    method: "PUT",
+    headers: { "Content-type" : "application/json" },
+    body: JSON.stringify(project) 
+  }));
 }
 
-export const deleteProject = (projectId: number) => {
-
+export const deleteProject = (projectId: string) => {
+  return Promise.resolve(fetch(`/projects/${projectId}`, { method: "DELETE" }));
 }
